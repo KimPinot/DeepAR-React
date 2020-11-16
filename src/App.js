3dcaaf005bb198144a8d57b0c2629352dd92595e
Pinot. Kim <pinot.kim@kakao.com>
2020-11-17 04:33:22 +0900

    :sparkles: Add DeepAR Feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const {DeepAR, innerHeight, innerWidth} = window;
+
+	const canvasRef = useRef(null);
+
+	useEffect(() => {
+		const deepAR = DeepAR({
+			canvasWidth: innerWidth,
+			canvasHeight: innerHeight,
+			licenseKey: process.env.REACT_APP_DEEPAR_KEY,
+			canvas: canvasRef.current,
+			numberOfFaces: 1,
+			libPath: '/lib',
+			segmentationInfoZip: 'segmentation.zip',
+			onInitialize: () => {
+				console.log('deepAR Ready');
+
+				deepAR.startVideo(true);
+
+				deepAR.switchEffect(0, 'slot', '/effects/flowers', () => {
+					console.log('flower loaded');
+				})
+			}
+		});
+
+		deepAR.downloadFaceTrackingModel('/lib/models-68-extreme.bin');
+	}, [DeepAR, innerHeight, innerWidth]);
+
+	return (
+			<>
+				<canvas ref={canvasRef} />
+			</>
+	);
 }
 
 export default App;
